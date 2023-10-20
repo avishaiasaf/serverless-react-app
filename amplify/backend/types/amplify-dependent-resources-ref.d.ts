3aea11fc9d1e89095f3739c5e96414d84d2f7bb3
Avishai Asaf <avishaiasaf@Avishais-MacBook-Pro.local>
2023-10-20 20:16:18 +0300

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "serverlessreactapp809fcc77": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
